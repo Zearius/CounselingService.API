7630c840f10c376f28260f9e327749eb855f7a6c
Andrew <andrewjason2525@gmail.com>
2023-10-15 20:11:55 -0700

    - Corrected incorrect attribute initilization for CounselingController. - Implemented Child COmponents of Counseling Objects.

--- CounselingService.API/Models/CounselingDTO.cs
+++ CounselingService.API/Models/CounselingDTO.cs
@@ -3,9 +3,19 @@
     public class CounselingDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Counselor { get; set; }   
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Counselor { get; set; } = string.Empty;   
+        public string? Description { get; set; }
         
+        public int NumberOfSpecialEvents
+        {
+            get
+            {
+                return SpecialEvents.Count;
+            }
+        }
+
+        public ICollection<SpecialEventsDTO> SpecialEvents { get; set; }
+           = new List<SpecialEventsDTO>();
     }
 }
