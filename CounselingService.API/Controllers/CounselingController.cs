d85319b9744ed500e03c6fb24373ca2d1b3eb75b
Andrew <andrewjason2525@gmail.com>
2023-10-15 19:10:20 -0700

    Add project files.

---
+++ CounselingService.API/Controllers/CounselingController.cs
@@ -0,0 +1,31 @@
+﻿using CounselingService.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CounselingService.API.Controllers
+{
+    [ApiController]
+    [Route("api/CounselingServices")]
+    public class CounselingController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCounselingServices()
+        {
+            return new JsonResult(CounselingDataStore.Current.CounselingServices);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CounselingDTO> GetCounselingService(int id) 
+        {
+            //Returns city & Validates Response
+            var CounselingToReturn = CounselingDataStore.Current.CounselingServices.FirstOrDefault(c => c.Id == id);
+            
+            //Returns Not Found, if ID does not exist.
+            if (CounselingToReturn == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(CounselingToReturn);
+        }
+    }
+}
---
+++ CounselingService.API/CounselingDataStore.cs
@@ -0,0 +1,40 @@
+﻿using CounselingService.API.Models;
+
+namespace CounselingService.API
+{
+    public class CounselingDataStore
+    {
+
+        public List<CounselingDTO> CounselingServices { get; set; }
+
+        public static CounselingDataStore Current { get; } = new CounselingDataStore();
+
+        public CounselingDataStore() 
+        { 
+            CounselingServices = new List<CounselingDTO>()
+            {
+                new CounselingDTO()
+                {
+                    Id = 1,
+                    Name = "Alcholics Anonymous",
+                    Counselor = "Sarah Johnson",
+                    Description = "Bi-weekly group, designed to support those recovering form Alcohol Addiction."
+                },
+                new CounselingDTO()
+                {
+                    Id = 2,
+                    Name = "Narcotics Anonymous",
+                    Counselor = "Brian Brackett",
+                    Description = "Bi-weekly group, designed to support those recovering from Narcotics Addiction."
+                },
+                new CounselingDTO()
+                {
+                    Id = 3,
+                    Name = "Gambelers Anonymous",
+                    Counselor = "Catherine Forestrad",
+                    Description = "Bi-weekly group, designed to support those recovering from Gambling Addiction."
+                }
+            };
+        }
+    }
+}
