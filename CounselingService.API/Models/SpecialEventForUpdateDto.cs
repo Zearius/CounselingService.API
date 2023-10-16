a3b2642fb800ccbb1c4f82b5da3922448f432d0b
Andrew <andrewjason2525@gmail.com>
2023-10-15 23:43:39 -0700

    Implemented Put, Patch, and Delete for basic CRUD operations.

---
+++ CounselingService.API/Models/SpecialEventForUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CounselingService.API.Models
+{
+    public class SpecialEventForUpdateDto
+    {
+        [Required(ErrorMessage = "You must provide a name.")]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+    }
+}
